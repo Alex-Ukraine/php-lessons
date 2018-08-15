fedf1ae24325e4ad1b98d9dcace79b8271fdbc3e
Number9 <Number9@gmail.com>
2018-08-15 19:19:27 +0300

    123,fps lfyys[

---
+++ baza-d/index.php
@@ -0,0 +1,40 @@
+<?php
+$con=mysqli_connect("localhost","root","","testsite");
+mysqli_set_charset($con, "utf8");
+
+// Check connection
+if (mysqli_connect_errno()) {
+	echo "Failed to connect to MongoDB: ".mysqli_connect_error();
+}
+
+//$query = "INSERT INTO news_category VALUES (NULL, 'Категория 3', 'Описание 2', 1,1);";
+
+//$info = mysqli_query($con, $query);
+
+//$query = "DELETE FROM news WHERE 'date' < DATE_SUB(CURDATE(), INTERVAL 2 MINUTE);" ; 
+
+// DESC - descending (убывание), ASC - ascending (возрастание)
+
+//$query = "DELETE FROM news_category ORDER BY id DESC LIMIT 5;";
+
+//$query = "UPDATE news SET status='1';" ;
+
+//$query = "UPDATE news SET h1='новость 3' WHERE h1='новость 1';";
+
+$query = " SELECT * FROM news; ";
+
+$info = mysqli_query($con, $query);
+
+$count=mysqli_num_rows($info);
+
+$row1 = mysqli_fetch_array($info); //возвращает одну строку
+
+print_r($row1);
+
+echo "<br>".$count."<br>";
+
+
+
+var_dump($info);
+
+echo mysqli_affected_rows($con);
--- form.php
+++ form.php
@@ -12,14 +12,17 @@
 <head>
 	<title>Урок№8, Формы</title>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <form action="form.php" method="get">
+	<?php var_dump($_POST); var_dump($_FILES); ?>
+	<form action="form.php" method="post" enctype="multipart/form-data">
+    <!--<form action="form.php" method="get">-->
 	A: <input type="text" name="a"><br>
 	B: <input type="text" name="b"><br>
 	C: <input type="text" name="c"><br>
 	<input type="submit" value="ОТправить" name="Submit">
+	<input type="file" name="file">
 </form>
 </body>
 </html>
